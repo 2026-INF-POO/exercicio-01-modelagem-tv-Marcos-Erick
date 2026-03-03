4caeb1ad3a75055b8256f1f7ceb88cec87673129
Marcos-Erick <20251ti0089@ifba.edu.br>
2026-03-03 09:10:11 -0300

    Update TV.java
    incompleto, mas feito

--- TV.java
+++ TV.java
@@ -1,1 +1,34 @@
-
+class TV{
+  int polegadas;
+  int volume=5;
+  string marca;
+  int voltagem;
+  int canal;
+  void ligar() {
+}    
+  void desligar() {
+}    
+  void aumentarvolume() {
+    if (volume<10) {
+      volume++;
+    }
+    return volume;
+}    
+  void diminuirvolume() {
+     if (volume>1) {
+      volume--;
+     }
+    return volume;
+  }
+  void mudarcanalcima() {
+    if (canal<5) {
+      canal++;
+    }
+    return canal;
+  }
+  void mudarcanalbaixo() {
+ if (canal>1) {
+      canal--;
+    }
+    return canal;
+  }
